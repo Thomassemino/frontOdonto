f9034be598efca1b4a593768d62366c9f5fd9108
Alejo <alejocstaf@proton.me>
2025-01-13 15:42:27 -0300

    sdas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  output: 'hybrid',  // Cambiado a hybrid
+  output: 'static',  // Cambiado a static
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
-    speedInsights: {
-      enabled: true,
-    },
     imageService: true,
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      domains: [],
-      formats: ['image/avif', 'image/webp'],
-    },
   }),
   integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['flowbite', 'flowbite-react']
+    }
+  }
 });
