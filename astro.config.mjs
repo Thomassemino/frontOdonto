c86c1d86e6afcba4816677496e2371573f2375e4
Alejo <alejocstaf@proton.me>
2025-01-13 17:53:00 -0300

    LayoutReadme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel'; // Cambia esta línea
+import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ['flatpickr'] 
+    }
+  }
 });
