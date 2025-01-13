385703938ffaf076b8ec536d3e3dc4351bb4b38b
Alejo <alejocstaf@proton.me>
2025-01-13 15:56:38 -0300

    dasd

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,27 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CxXfw3ux.mjs';
+import { manifest } from './manifest_J4RYYu7f.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "45874edc-e401-4807-bd98-08f38abfac3b",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_S_kREODw.mjs';
+export { renderers } from '../renderers.mjs';
