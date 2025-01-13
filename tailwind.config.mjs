7701e2e4599d7ba3f3ad27dfdc19b609709c09be
Alejo <alejocstaf@proton.me>
2025-01-13 16:27:10 -0300

    dfsd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
-	],
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')({
-			datatables: true,
-		}),
-	],
+        require('flowbite/plugin')
+    ]
 }
