6bde63b97393b920af6c097a43fc10a8e09eb53a
Alejo <alejocstaf@proton.me>
2025-01-13 16:01:59 -0300

    sdad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    // Agrega la configuración de Flowbite
+    config: {
+      content: [
+        "./node_modules/flowbite/**/*.js",
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+      ],
+      plugins: [
+        require('flowbite/plugin')
+      ]
+    }
+  })],
   output: 'server',
   adapter: vercel(),
 });
