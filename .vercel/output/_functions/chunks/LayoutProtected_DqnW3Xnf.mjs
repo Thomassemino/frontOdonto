abca5cccc2c4a9a544ad388e3fafb648149adc8a
Alejo <alejocstaf@proton.me>
2025-01-13 15:31:57 -0300

    fdsf

---
+++ .vercel/output/_functions/chunks/LayoutProtected_DqnW3Xnf.mjs
@@ -0,0 +1,9 @@
+import { c as createComponent, r as renderTemplate, a as renderScript, b as renderSlot } from './astro/server_BR06E_5U.mjs';
+import 'kleur/colors';
+import 'clsx';
+
+const $$LayoutProtected = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${renderScript($$result, "C:/Users/Windows/Documents/Odontograma/Consultorio-Odontologico-FRONTEND/src/app/LayoutProtected.astro?astro&type=script&index=0&lang.ts")} ${renderSlot($$result, $$slots["default"])}`;
+}, "C:/Users/Windows/Documents/Odontograma/Consultorio-Odontologico-FRONTEND/src/app/LayoutProtected.astro", undefined);
+
+export { $$LayoutProtected as $ };
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    tailwind(),
-  ]
+  integrations: [tailwind()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['flowbite/dist/flowbite.min.js'],
+      }
+    },
+    ssr: {
+      noExternal: ['flowbite-react']
+    },
+    optimizeDeps: {
+      exclude: ['@astrojs/vercel/serverless']
+    }
+  }
 });
